9a04100fe4a9b9ad6df1c8287684785d237fe62f
jidnesh <jidnesh.bhogare@gmail.com>
2018-10-21 16:44:30 +0530

    Adding a first version of emoji

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
+
+console.log("Hello World");
+
+ReactDOM.render(<App />, document.getElementById("root"));
